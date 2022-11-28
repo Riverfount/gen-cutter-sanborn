c2c38fcf573c889d1d0fb6d33a19abff33aff0d7
Vicente Marçal <vicente.marcal@gmail.com>
2022-11-28 17:14:42 -0300

    test(API): Implemented tests to cover gen_cutter_table function of business rules.
    Closes #14

---
+++ tests/test_business_rules.py
@@ -0,0 +1,15 @@
+import httpx
+from pytest_httpx import HTTPXMock
+
+from api.business_rules.cutter_table import gen_cutter_table
+
+
+def test_gen_cutter_table_works_ok(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(
+        text='<b>A</b><pre><b>    111     Aa\n    112     Aabb</b></pre>'
+    )
+
+    with httpx.Client():
+        response = gen_cutter_table()
+        assert response['a'] == [('111', 'Aa'.lower()), ('112', 'Aabb'.lower())]
+        assert True
